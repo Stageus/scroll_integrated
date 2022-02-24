5c9e78356c1c77f5e93762d09fbea2abc9d2ef0b
ganadaramaba <cartoon_____@naver.com>
2022-02-24 07:04:47 +0000

    UPDATE : server.js basic setting

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3000; // 나중에 수정
 const path = require("path");
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.listen(port, (req, res) => {
+    console.log(port, "번에 서버 실행");
+});
