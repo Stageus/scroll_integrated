1ecef2df7e8d1001d756c86007e12f738a10ada5
ganadaramaba <cartoon_____@naver.com>
2022-03-06 19:59:37 +0900

    testtest

--- router/connectTest.js
+++ router/connectTest.js
@@ -45,7 +45,7 @@ router.post("/elastic", (req, res) => {
     console.log(1)
 
     const client = new es.Client({
-        node: "http://elasticsearch"
+        node: "http://localhost:9200"
     });
 
     console.log(1)
