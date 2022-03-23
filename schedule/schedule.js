4dd950aa10e53c4f3cdc9a9fc21fa2ce586cc1f9
Ubuntu <ubuntu@ip-172-31-37-69.ap-northeast-2.compute.internal>
2022-03-23 12:56:34 +0000

    UPDATE: schedule.js fix time minute to day

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -3,7 +3,8 @@ const schedule = require("node-schedule");
 const crawler = require("../module/crawler");
 const uploadRedis = require("../module/redisUploader")
 
-const demonizer = () => schedule.scheduleJob("0 * * * * *", async() => {
+// const demonizer = () => schedule.scheduleJob("0 * * * * *", async() => {
+const demonizer = () => schedule.scheduleJob("0 0 17 * * *", async() => {
     console.log("second");
     // await crawler();
     // await uploadRedis();
